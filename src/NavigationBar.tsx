4d32d519cc49a771ec4df920aa2a1d330dd9b5ac
Claiko <jonathan.dellova42@gmail.com>
2025-07-20 17:15:52 +0200

    Added highlighting animation around elements reached from navbar.

--- src/NavigationBar.tsx
+++ src/NavigationBar.tsx
@@ -13,10 +13,21 @@ export type NavbarProps = {
 	className?: string;
 }
 
+function HighlightElement(element: HTMLElement) {
+	const animClassName = 'highlight-pulse-active';
+
+	element.classList.add(animClassName);
+
+	// After the class is added, add an event listener that will trigger once the animation is ended and remove tha class.
+	element.addEventListener('animationend', () => {
+		element.classList.remove(animClassName);
+	}, { once: true });
+}
+
 export default function NavigationBar({ items, className = '' }: NavbarProps) {
 	const navbarRef = useRef<HTMLElement>(null);
 
-	const handleNavClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>, href: string) => {
+	const onNavClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>, href: string) => {
 		event.preventDefault(); // Override default behavior of anchor click.
 
 		// Find the DOM element that matches href. 
@@ -25,9 +36,30 @@ export default function NavigationBar({ items, className = '' }: NavbarProps) {
 		if (targetElement instanceof HTMLElement && navbarElement) {
 			const navbarHeight = navbarElement.offsetHeight;
 
+			// When the element has scrolled into the viewport it should be highlighted and the observer 
+			// should be disconnected.
+			const onIntersect: IntersectionObserverCallback = (entries) => {
+				entries.forEach(entry => {
+					if (entry.isIntersecting) {
+						HighlightElement(entry.target as HTMLElement);
+						intersectionObserver.disconnect();
+					}
+				})
+			};
+
+			// Accounts for navbar and makes sure to trigger once the element is partly in view.
+			const intersectObserverOptions: IntersectionObserverInit = {
+				threshold: 0.2,
+				rootMargin: `-${navbarHeight}px 0px 0px 0px`
+			}
+
+			const intersectionObserver = new IntersectionObserver(onIntersect, intersectObserverOptions);
+			intersectionObserver.observe(targetElement);
+
 			// Dynamically find a scroll pos that does not cover the content.
-			// Added 20 pixels as a forced margin.
-			const scrollPos = targetElement.offsetTop - navbarHeight - 20;
+			// Added buffer pixels as a forced margin.
+			const offsetBuffer = 40;
+			const scrollPos = targetElement.offsetTop - navbarHeight - offsetBuffer;
 
 			// Element specific method to smoothly scroll it into view so 
 			// the start of the block is at top of webpage.
@@ -47,7 +79,7 @@ export default function NavigationBar({ items, className = '' }: NavbarProps) {
 						<a
 							href={item.href}
 							className='navbar__link'
-							onClick={(e) => handleNavClick(e, item.href)} // Catch the event and send it to custom event handler.
+							onClick={(e) => onNavClick(e, item.href)} // Catch the event and send it to custom event handler.
 						>
 							{item.label}
 						</a>
